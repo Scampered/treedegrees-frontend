a65ba92d2f4fea08971a5f169ee3d43eeda3d3a6
Scampered <scamperedcraft99@gmail.com>
2026-03-16 05:09:23 +0300

    switch to carto dark tiles in english

--- src/pages/MapPage.jsx
+++ src/pages/MapPage.jsx
@@ -142,8 +142,8 @@ export default function MapPage() {
           worldCopyJump
         >
           <TileLayer
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/">CARTO</a>'
           />
 
           {flyTo && <FlyTo center={flyTo} />}
